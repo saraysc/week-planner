ee4315b984a9633fb51c0a81c85530a0de1fc7e6
Sara Choi <sarachoi10@hotmail.com>
2022-10-31 21:22:56 +0000

    message

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function popUp(event) {
   clickCount++;
   if (clickCount % 2 !== 0) {
     $entryModal.classList.remove('hidden');
-    $container2.className = 'black';
+    $container2.classList.add('black');
   }
 }
 
@@ -14,3 +14,10 @@ $addEntry.addEventListener('click', popUp);
 var $entryModal = document.querySelector('.modal');
 
 var $submit = document.querySelector('button.row');
+
+$submit.addEventListener('click', close);
+
+function close(event) {
+  $entryModal.classList.add('hidden');
+  $container2.classList.remove('black');
+}
