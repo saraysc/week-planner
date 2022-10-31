0b996b53e3844d56bdffa112d8d5312253ba47bc
James Yang <jamesdaeyang@gmail.com>
2022-10-31 21:01:22 +0000

    Added background color

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var clickCount = 0;
+var $container2 = document.querySelector('.container2');
 
 function popUp(event) {
   clickCount++;
   if (clickCount % 2 !== 0) {
     $entryModal.classList.remove('hidden');
+    $container2.className = 'black';
   }
 }
 
 var $addEntry = document.querySelector('.buttondiv');
 $addEntry.addEventListener('click', popUp);
 var $entryModal = document.querySelector('.modal');
+
+var $submit = document.querySelector('button.row');
